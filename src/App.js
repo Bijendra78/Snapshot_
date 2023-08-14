09f38202b6ee98fa19f869cb6e9949b74a0894ba
Bijendra78 <135230949+Bijendra78@users.noreply.github.com>
2023-08-14 20:03:02 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Photo from './Photo/GetPhot';
+
+function App() {
+  return (
+    <div className="App">
+     <Photo/>
+    </div>
+  );
+}
+
+export default App;
